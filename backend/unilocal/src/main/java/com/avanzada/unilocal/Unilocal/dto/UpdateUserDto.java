99556714a7458ff930d75b9acb9bdec9a020620a
Juankart65 <104481229+Juankart65@users.noreply.github.com>
2024-05-08 00:50:34 -0500

    Update UpdateUserDto to accept generic type for photo field

--- backend/unilocal/src/main/java/com/avanzada/unilocal/Unilocal/dto/UpdateUserDto.java
+++ backend/unilocal/src/main/java/com/avanzada/unilocal/Unilocal/dto/UpdateUserDto.java
@@ -3,12 +3,12 @@ package com.avanzada.unilocal.Unilocal.dto;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
-public record UpdateUserDto(
+public record UpdateUserDto<T>(
         @NotBlank(message = "name is required")
         @Length(min = 7, max = 50)
         String name,
         @NotBlank(message = "photo is required")
-        String photo,
+        T photo,
         @NotBlank(message = "nickname is required")
         String nickname,
         @NotBlank(message = "email is required")
