2f7bfd7059fe909d81b3d0cd643e54bc20a6fbd1
Juankart65 <104481229+Juankart65@users.noreply.github.com>
2024-05-18 21:29:46 -0500

    Create new components frontend
    -The components are by mod, detailLocal, forgot password and change password.

---
+++ frontend/unilocal/src/app/map/components/dashboard-mod/dashboard-mod.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-mod',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-mod.component.html',
+  styleUrl: './dashboard-mod.component.css'
+})
+export class DashboardModComponent {
+
+}
---
+++ frontend/unilocal/src/app/map/components/detail-local/detail-local.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-local',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-local.component.html',
+  styleUrl: './detail-local.component.css'
+})
+export class DetailLocalComponent {
+
+}
